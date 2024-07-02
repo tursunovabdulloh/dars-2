182bf0d1958a34a77d73973eab0524a770488b92
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-07-02 15:50:01 +0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.openmoviedb.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
---
+++ src/components/movieItem/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function movieItem() {
+  return <div>movieItem</div>;
+}
