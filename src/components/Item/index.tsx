236a956a72688bc151283c997d89664db63adb27
Abdulloh <email@example.com>
2024-07-16 15:32:42 +0500

    .

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -44,7 +44,7 @@ export default function Item({ data }: { data: Movie }) {
         data,
       ];
       localStorage.setItem("favourites", JSON.stringify(newFavs));
-      message.success(`${type} has been bookmarked!`);
+      message.success(`${name ?? alternativeName} has been bookmarked!`);
     } else {
       const newFavs = fav.filter((item) => item.id !== fav[status].id);
       localStorage.setItem("favourites", JSON.stringify(newFavs));
@@ -98,8 +98,8 @@ export default function Item({ data }: { data: Movie }) {
               className={style.playIcon}
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM10.6935 15.8458L15.4137 13.059C16.1954 12.5974 16.1954 11.4026 15.4137 10.941L10.6935 8.15419C9.93371 7.70561 9 8.28947 9 9.21316V14.7868C9 15.7105 9.93371 16.2944 10.6935 15.8458Z"
                 fill="currentColor"
               />
